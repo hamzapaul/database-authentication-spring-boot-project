d45f77e6b3ab36ca38dc5a8363c50e0cb82dda2c
hamzapaul <73592443+hamzapaul@users.noreply.github.com>
2022-07-24 00:50:25 +0200

    project generated

---
+++ src/main/java/paul/com/databaseauth/DatabaseAuthApplication.java
@@ -0,0 +1,13 @@
+package paul.com.databaseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paul/com/databaseauth/DatabaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package paul.com.databaseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
